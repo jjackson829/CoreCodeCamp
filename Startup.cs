8337e7673a3ed3f335dc17189c0f88cc952f608c
Jere Jackson <jere.j@comcast.net>
2021-03-02 18:40:48 -0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Controllers;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+namespace CoreCodeCamp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CampContext>();
+            services.AddScoped<ICampRepository, CampRepository>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddApiVersioning(opt =>
+            {
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 1);
+                opt.ReportApiVersions = true;
+                //opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+                opt.ApiVersionReader = ApiVersionReader.Combine(
+                  new HeaderApiVersionReader("X-Version"),
+                  new QueryStringApiVersionReader("ver", "version"));
+
+                //opt.Conventions.Controller<TalksController>()
+                //  .HasApiVersion(new ApiVersion(1, 0))
+                //  .HasApiVersion(new ApiVersion(1, 1))
+                //  .Action(c => c.Delete(default(string), default(int)))
+                //    .MapToApiVersion(1, 1);
+
+            });
+
+            services.AddControllers();
+
+            services.AddMvc(opt => opt.EnableEndpointRouting = false)
+              .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllers();
+            });
+        }
+    }
+}
